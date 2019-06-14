2b8bdb7318580a87b1eaeaadaf892dad4ece82af
João Paulo Ferreira de Souza <joao.ferreiradesouza@hotmail.com>
2019-06-13 23:12:43 -0300

    Plot Graph

--- Aula1/ExPlot/main.js
+++ Aula1/ExPlot/main.js
@@ -14,7 +14,27 @@ var parameters = {
 };
 
 function plot() {
+    var xMin = document.querySelector("#xMin").value;
+    var xMax = document.querySelector("#xMax").value;
+
+    var yMin = document.querySelector("#yMin").value;
+    var yMax = document.querySelector("#yMax").value;
+
+    var func = document.querySelector("#function").value;
+
     var color = document.querySelector("#color").value;
+
+    parameters.xAxis.domain = [xMin, xMax];
+    parameters.xAxis.domain = [yMin, yMax];
+   
     parameters.data[0].color=color;
+
+    if(func == ""){
+        parameters.data[0].fn = 'sin(x)';
+    }else{
+        parameters.data[0].fn = func;
+    }
+
+
     functionPlot(parameters);
 }
